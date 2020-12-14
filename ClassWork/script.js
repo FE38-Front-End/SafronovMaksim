c660f7ea73452ae78cf009fe4227484190fa7ad5
SafronovMaksim <wow_circle23@mail.ru>
2020-12-14 16:58:27 +0300

    change2

--- ClassWork/script.js
+++ ClassWork/script.js
@@ -1,4 +1,4 @@
-//Создание квадрата и окружности
+//Создание квадрата
 const addSquare = document.getElementById('squareApply');
 let squareHidth;
 let square;
@@ -20,17 +20,29 @@ addSquare.addEventListener('click', () => {
         alert('Вы создали квадрат');
     }
 });
+
+
+function getValue(name) {
+    return localStorage.getItem(name) || '';
+}
+squareWidth.addEventListener('change', () => {
+    localStorage.setItem('squareWidth', squareWidth.value);
+});
+squareWidth = getValue('squareWidth');
+
+//Кнопка удаления квадрата
 const deleteSquare = document.getElementById('squareClear');
 deleteSquare.addEventListener('click', () => {
-    let delSquare = document.querySelectorAll('div');
-    if(delSquare.length > 0){
+    let delSquare = document.querySelector('div');
+    if(delSquare){
         delSquare.parentNode.removeChild(delSquare);
+        square = null;
     }else{
         alert('Квадрат отсутствует');
     }
 });
 
-
+//Создание круга
 const addCircle = document.getElementById('circleApply');
 let circle;
 addCircle.addEventListener('click', () => {
@@ -59,46 +71,67 @@ addCircle.addEventListener('click', () => {
         alert('Сперва создайте квадрат');
     }
 })
+//Кнопка удаления круга
+const deleteCircle = document.getElementById('circleClear');
+deleteCircle.addEventListener('click', () => {
+    let delCircle = document.querySelector('div div');
+    if(delCircle){
+        delCircle.parentNode.removeChild(delCircle);
+        circle = null;
+    }else{
+        alert('Круг отсутствует');
+    }
+});
 //Создание интерфейса
+let right = document.getElementById('right');
+right.addEventListener('click', moveRight);
 function moveRight(){
     let left = circle.style.left;
     left = parseInt(left);
-    if(left < squareWidth/2){
+    if(left < squareWidth/2-circleWidth/2){
         left += parseInt(document.getElementById('step').value);
         circle.style.left = left +'px';
+    }else{
+        right.disabled='true';
     }
 }
+left = document.getElementById('left');
+left.addEventListener('click',moveLeft);
 function moveLeft(){
     let left = circle.style.left;
     left = parseInt(left);
-    if(left > -squareWidth/2){
+    if(left > -squareWidth/2+ circleWidth/2){
         left -= parseInt(document.getElementById('step').value);
         circle.style.left = left +'px';
-    }
-}
-let right = document.getElementById('right');
-    left = document.getElementById('left');
-right.addEventListener('click',moveRight);
-left.addEventListener('click',moveLeft);
-
-function moveDown(){
-    let down = circle.style.top;
-    down = parseInt(down);
-    if(down < squareWidth/2){
-        down += parseInt(document.getElementById('step').value);
-        circle.style.top = down +'px';
-    }
-}
-function moveUp(){
-    let down = circle.style.top;
-    down = parseInt(down);
-    if(down > -squareWidth/2){
-        down -= parseInt(document.getElementById('step').value);
-        circle.style.top = down +'px';
+    }else{
+        left.disabled = 'true';
     }
 }
 let down = document.getElementById('down');
-    up = document.getElementById('up');
 down.addEventListener('click',moveDown);
+function moveDown(){
+    let down = circle.style.top;
+    down = parseInt(down);
+    if(down < squareWidth/2 - circleWidth/2){
+        down += parseInt(document.getElementById('step').value);
+        circle.style.top = down +'px';
+    }else{
+        down.disabled = 'true';
+    }
+}
+up = document.getElementById('up');
 up.addEventListener('click', moveUp);
+function moveUp(){
+    let down = circle.style.top;
+    down = parseInt(down);
+    if(down > -squareWidth/2 +circleWidth/2){
+        down -= parseInt(document.getElementById('step').value);
+        circle.style.top = down +'px';
+    }else{
+        up.disabled = 'true';
+    }
+}
 
+    
+
+// let q = document.getElementById('down').disabled ='true';
